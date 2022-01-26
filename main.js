a95f05bb7dad8720c9c22f693c3db00a438db223
antoniogiachin <giachinantonio@outlook.com>
2022-01-26 17:14:57 +0100

    inizio lavoro JS

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 // Creo 3 array
-const imgMain = ['img/01.jpg' ,'img/02.jpg' , 'img/03.jpg' , 'img/04.jpg' , 'img/05.jpg' , ];
+const items = ['img/01.jpg' ,'img/02.jpg' , 'img/03.jpg' , 'img/04.jpg' , 'img/05.jpg' , ];
 
-const imgCountry = ['Svezia' , 'Svizzera' , 'Gran Bretagna' , 'Germania' , 'Paradise']; 
+const title = ['Svezia' , 'Svizzera' , 'Gran Bretagna' , 'Germania' , 'Paradise']; 
 
-const imgText = [
+const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
     'Lorem ipsum',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
+
+//Foto
+let mainImg;
+
+//Paese
+let mainCountry;
+
+//Testo
+let mainText;
+
+// Ciclo immagini
+for (let i = 0; i < items.length; i++){
+    mainImg += `<div class="img-big"> <img src="${items[i]}> </div>`
+    console.log('mainImg')
+
+}
